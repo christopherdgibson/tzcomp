fd6324c48580413c061416c986636a5434fb48a1
Chris <christopher.d.gibson1@gmail.com>
2026-06-11 11:16:03 +0200

    Adjust web scroll view styles #tzcomp

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { Platform , ScrollView, StyleSheet, View } from 'react-native';
+import { Platform, StyleSheet, View } from 'react-native';
 import { useFonts } from 'expo-font';
 // import { SafeAreaView } from 'react-native-safe-area-context';
 import {ThemeProvider} from '@/contexts/theme-context';
@@ -7,11 +7,8 @@ import {SettingsProvider} from '@/contexts/settings-context';
 import TimeZoneApp from "@/components/TimeZoneApp";
 import timeZoneData from "@/constants/time-zone-names.json";
 const timeZoneNames = timeZoneData.timeZoneNames;
-import { useTheme } from '@/hooks/use-theme';
 
 export default function App() {
-      const theme = useTheme();
-      const styles = makeStyles(theme);
     const [fontsLoaded] = useFonts({
     'AvenirLTStdLight': require('./assets/fonts/AvenirLTStdLight.otf'),
     'AvenirLTStdBook': require('./assets/fonts/AvenirLTStdBook.otf'),
@@ -24,32 +21,23 @@ export default function App() {
     //       </SafeAreaView>
     <SettingsProvider>
       <ThemeProvider>
-        <ScrollView style={styles.cardContainer}>
-          <View style={styles.container}>
-            <TimeZoneApp timeZoneNames={timeZoneNames} />
-            <StatusBar style="auto" />
-          </View>
-        </ScrollView>
+        <View style={styles.container}>
+          <TimeZoneApp timeZoneNames={timeZoneNames} />
+          <StatusBar style="auto" />
+        </View>
+        <View>Download APK</View>
       </ThemeProvider>
     </SettingsProvider>
   );
 }
-const makeStyles = (theme: ReturnType<typeof useTheme>) =>
-StyleSheet.create({
+
+const styles = StyleSheet.create({
   container: {
-      flex: 1,
-      backgroundColor: theme.bgContainer,
-      alignItems: 'center',
-      width: Platform.OS === 'web' ? undefined : '100%',
-      maxWidth: Platform.OS === 'web' ? 480 : undefined,
-      alignSelf: 'center',
-  },
-  cardContainer: {
-    position: 'relative',
-    width: '100%',
-    backgroundColor: theme.bgContainer,
-    marginTop: 0,
-    margin: 'auto',
-    color: theme.fontColor,
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginHorizontal: 'auto',
+    width: Platform.OS === 'web' ? undefined : '100%',
+    maxWidth: Platform.OS === 'web' ? 480 : undefined,
   },
 });
